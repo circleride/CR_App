e825cba0f3195538d78167f95c5316e6d4555cce
maitret <maitret@myhostmx.com>
2017-10-20 17:51:52 -0500

    Nothing to do here! :)

--- js/header_web.js
+++ js/header_web.js
@@ -97,7 +97,7 @@ var Base64 = {
         }
         return t
     }
-}
+};
 function guid() {
     function s4() {
         return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
