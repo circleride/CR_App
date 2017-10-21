7208a64599dd4beed151ec90cbc4aa402c031f99
maitret <maitret@myhostmx.com>
2017-10-21 16:50:50 -0500

    Nothing to do here! :)

--- js/pg_index.js
+++ js/pg_index.js
@@ -1,7 +1,14 @@
 var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9+/=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/rn/g,"n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}
+function guid(){ function s4() { return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1); } return s4() + s4() + '-' + s4() + '-' + s4() + '-' + s4() + '-' + s4() + s4() + s4(); }
+var Usuario_SS = localStorage.getItem('usuario');
+var myUuid = localStorage.getItem('key');
+if (!myUuid) { myUuid = guid(); localStorage.setItem('key', myUuid); }
+if (!Data_Id) { Data_Id = "0"; }
+if (!Usuario) { Usuario = "Invitado"; }
+if (!Usuario_md5) { Usuario_md5 = "0"; }
 
 var url_server_firebase = localStorage.getItem('url_server_firebase');
-var url_server_firebase_def = "https://www.gstatic.com/firebasejs/4.5.0/firebase.js";
+var url_server_firebase_def = "https://www.gstatic.com/firebasejs/4.6.0/firebase.js";
 if (!url_server_firebase) {
 window.url_server_firebase = url_server_firebase_def;
 localStorage.setItem('url_server_firebase', url_server_firebase_def);
@@ -62,18 +69,6 @@ $("input[name=Lon]").val(latLng.lng());
 });
 };
 
-window.enable_gallery = function enable_gallery(class_lg){
-if(class_lg == "" || class_lg == "undefined" || class_lg == null){ class_lg = ".gallery_lg"; }
-$(".gallery_lg").lightGallery({
-thumbnail:false,
-animateThumb: false,
-showThumbByDefault: false,
-fullScreen: false,
-download: false,
-hash: false
-});
-};
-
 window.handle_url = "";
 function handleOpenURL(url){
 setTimeout(function() {
@@ -98,17 +93,17 @@ hash: false
 window.enable_gallery();
 });
 
-window.url_target = function url_target(page,id){
-if(id == "undefined" || id == null || id == ""){ id = ""; }
-if(page == "undefined" || page == null || page == ""){ page = ""; }
-//alert(page+" : "+id);
-$('.content_main').html('<div align="center"><br><br><h4><i class="fa fa-spinner fa-spin"></i></h4><br></div>');
-$.getJSON(window.url_server+"/movil/html.templates.php", { key: window.my_uuid, html: page, id: id, view_as: 'json', is_app: '1' }, function (j) {
-var stateObj = { html: page };
-var data_html = j['content'];
-$(".content_main").html(data_html);
-$(".navigation-trigger, .sidebar").removeClass("toggled"); $(".ma-backdrop").remove();
-});
+window.url_target = function url_target(url) {
+    $(".navigation-trigger, .sidebar").removeClass("toggled");
+    $(".ma-backdrop").remove();
+    $('.content_main').html('<div align="center"><br><br><h4><i class="fa fa-spinner fa-spin"></i> Cargando...</h4><br><br><br></div>');
+    var stateObj = {html: url};
+    history.pushState(stateObj, "", url_server + "/ver/" + url);
+    $('.content_main').load(url_server + '/_/' + url + "");
+    window.onpopstate = function (e) {
+        e.preventDefault();
+        window.url_target("");
+    };
 };
 
 window.div_user_menu = function (){
@@ -119,3 +114,8 @@ var data_html = j['content'];
 $(".div_user_menu").html(data_html);
 });
 };
+
+$(document).ready(function () {
+    $("[rel=tooltip]").tooltip();
+    $("[rel=popover]").popover();
+});
